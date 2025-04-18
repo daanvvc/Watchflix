9d2118962c30ce39f61ffcc037a786bff2d95fa1
Daan <daanvervaecke@gmail.com>
2025-04-18 12:55:08 +0200

    Allow origins from any source

--- MovieFileService/src/main/java/fontys/IA/configuration/WebConfig.java
+++ MovieFileService/src/main/java/fontys/IA/configuration/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/movieFile/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
