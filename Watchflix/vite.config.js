f1fa063616d194318268124527c7e74e5f06a09f
Daan <daanvervaecke@gmail.com>
2025-04-18 17:32:15 +0200

    Updated pipeline

--- Watchflix/vite.config.js
+++ Watchflix/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom', 
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
-
