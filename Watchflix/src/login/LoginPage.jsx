33651bc9f1a89f1f8054a8dc3484b126eef213f0
Daan <daanvervaecke@gmail.com>
2025-05-13 20:04:37 +0200

    Test

--- Watchflix/src/login/LoginPage.jsx
+++ Watchflix/src/login/LoginPage.jsx
@@ -1,16 +1,25 @@
 import { useEffect } from "react";
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useNavigate } from "react-router-dom";
 
 function LoginPage(props) {
+    const navigate = useNavigate();
+
     useEffect(() => {
         props.supabaseClient.auth.getSession().then(({ data: { session } }) => {
         props.setSession(session)
+        if (session) {
+          navigate('/dashboard'); 
+        }
       })
       const { data: { subscription } } = props.supabaseClient.auth.onAuthStateChange((_event, session) => {
       try {
         console.log("Auth state changed");
         props.setSession(session);
+        if (session) {
+          navigate('/dashboard'); 
+        }
       } catch (error) {
         console.error('Error handling auth state change:', error);
       }
