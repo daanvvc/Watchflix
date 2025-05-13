e376d1d420bd565a8e297d0a46a7952e32bb5bd3
Daan <daanvervaecke@gmail.com>
2025-05-13 18:45:17 +0200

    Bug fix

--- EventBus/src/main/java/fontys/IA/MovieUploadRunner.java
+++ EventBus/src/main/java/fontys/IA/MovieUploadRunner.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/movieFile")
